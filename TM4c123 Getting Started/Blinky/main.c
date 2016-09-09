3d2467e4b2be91dae5c52d7d6f544d4a3b7570df
Shriya <sshriya7@vt.edu>
2016-09-09 15:54:46 -0400

    Added getting strated codes

---
+++ TM4c123 Getting Started/Blinky/main.c
@@ -0,0 +1,59 @@
+/* %%%%%%%%%%%%  DIGITAL OUTPUT  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+ * ------------Blink Blue LEd on LaunchPAd
+ * */
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_types.h"
+#include "inc/hw_memmap.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+
+#define USER_LED  GPIO_PIN_1
+
+int main(void)
+{
+    //
+    // Setup the system clock to run at 50 Mhz from PLL with crystal reference
+    //
+    SysCtlClockSet(SYSCTL_SYSDIV_4|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|
+                    SYSCTL_OSC_MAIN);
+
+    //
+    // Enable and wait for the port to be ready for access
+    //
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF))
+    {
+    }
+
+    //
+    // Configure the GPIO port for the LED operation.
+    //
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, USER_LED);
+
+    //
+    // Loop Forever
+    //
+    while(1)
+    {
+        //
+        // Turn on the LED
+        //
+        GPIOPinWrite(GPIO_PORTF_BASE, USER_LED, USER_LED);
+
+        //
+        // Delay for a bit
+        //
+        SysCtlDelay(2000000);
+
+        //
+        // Turn on the LED
+        //
+        GPIOPinWrite(GPIO_PORTF_BASE, USER_LED, ~(USER_LED));
+
+        //
+        // Delay for a bit
+        //
+        SysCtlDelay(2000000);
+    }
+}
